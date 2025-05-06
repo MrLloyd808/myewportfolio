fb880407cb488837e7bf3632a1ad77e6bd93b378
LIL-MANIAC-001 <127343644+MrLloyd808@users.noreply.github.com>
2025-05-06 16:21:16 +0200

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HeaderMobile from './Components/HeaderMobile'
+
+function App() {
+
+  return (
+    <>
+    <div className="App">
+      <HeaderMobile />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/HeaderMobile.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const HeaderMobile = () => {
+  return (
+   
+    <div className="header-mobile">
+        <h2>Portfolio</h2>
+
+        <div className='hamburger'>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+    </div>
+  )
+}
+
+export default HeaderMobile
