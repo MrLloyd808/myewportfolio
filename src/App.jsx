0b043e2b8fc539b968feadc7dd92761ce590c81b
LIL-MANIAC-001 <127343644+MrLloyd808@users.noreply.github.com>
2025-05-06 19:26:50 +0200

    mobile almost done

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,27 @@ import "./App.css"
 
 export default function HeaderMobile() {
   return (
-   
     <div className="header-mobile">
          <h2><b className='orangetext'>Port</b>folio</h2>
-
         <div className='hamburger' onClick={() => document.getElementById('NavBar').showModal()}>
             <span></span>
             <span></span>
             <span></span>
         </div>
-
         <dialog id='NavBar'>
         <div className='nav-top-wrapper'>
          <button className='closebtn' onClick={() => document.getElementById('NavBar').close()}>+</button>
-     <h3>
-     <b className='orangetext'>
-     Port
-     </b>folio
-     </h3>
-     
+           <h3>
+             <b className='orangetext'>Port</b>folio
+           </h3>
         </div>
-         
-          
-
+      <div className='nav-links-wrapper'>
+        <button className='nav-links'>About</button>
+        <button className='nav-links'>Technologies</button>
+        <button className='nav-links'>Social Links</button>
+      </div>  
         </dialog>
+     
     </div>
   )
 }
