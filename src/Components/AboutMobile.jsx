0acee7c127e34b1cebd949959a5a17817a45c943
LIL-MANIAC-001 <127343644+MrLloyd808@users.noreply.github.com>
2025-05-12 07:48:11 +0200

    styling is annoying

--- src/Components/AboutMobile.jsx
+++ src/Components/AboutMobile.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
-
+import url from "./assets/image2.jpg"
 export default function AboutMobile() {
   return (
 
     <section className='about-mobile'>
-      <h3>HELLO <b className='orange-text'>THERE!</b></h3>
-      <p>
-        I am a <b className='orange-text'>WEB DEVELOPER</b>.</p>
+      <h3 className='left'>HELLO <b className='orange-text'>THERE!</b></h3>
+      <p className='left'>
+        I am a WEB <b className='orange-text'>DEVELOPER</b>.</p>
         <p>
         <br/>
         Welcome to my <b className='orange-text'>Portfolio. <br/></b> 
         I am a self taught Programmer with a passion to learn and create. 
         Make youreself at home. Go through the rest of the Portfolio to learn more about myself.
       </p>
+      <img src={url} alt="About Me" className='my-img' /> 
+        
+      <hr/>
     </section>
+
+   
   )
 }
