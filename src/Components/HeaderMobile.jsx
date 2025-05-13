7f9ec164794c7c7990f1afe9142f154f199ca3e0
LIL-MANIAC-001 <127343644+MrLloyd808@users.noreply.github.com>
2025-05-13 05:00:24 +0200

    last commit of day

--- src/Components/HeaderMobile.jsx
+++ src/Components/HeaderMobile.jsx
@@ -8,26 +8,10 @@ const HeaderMobile = () => {
    
     <div className="header-mobile">
         <img src={img} alt="logo" className='logo'/>
-         <h2><b className='orangetext'>Port</b>folio</h2>
-        <div className='hamburger' onClick={() => document.getElementById('NavBar').showModal()}>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <dialog id='NavBar'>
-        <div className='nav-top-wrapper'>
-         <button className='closebtn' onClick={() => document.getElementById('NavBar').close()}>+</button>
-           <h3>
-             <b className='orangetext'>Port</b>folio
-           </h3>
-        </div>
-      <div className='nav-links-wrapper'>
+         <h3><b className='orangetext'>Port</b>folio</h3>
         <button className='nav-links'>About</button>
-        <button className='nav-links'>Technologies</button>
-        <button className='nav-links'>Social Links</button>
-      </div>  
-        </dialog>
-     
+        <button className='nav-links'>Skills</button>
+        <button className='nav-links hire-btn'>HIRE</button>
     </div>
   )
 }
